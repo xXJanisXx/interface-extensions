3533aff3da75dc873cebce65b2b0282fbc81f260
xXJanisXx <Janis.Minecrafto@gmx.de>
2026-01-01 23:05:41 +0100

    feat: added maven publishing

--- interfaces/build.gradle.kts
+++ interfaces/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java-library")
+    id("maven-publish")
 }
 
 dependencies {
@@ -10,4 +11,49 @@ dependencies {
 java {
     withJavadocJar()
     withSourcesJar()
+}
+
+
+publishing {
+    repositories {
+        maven {
+            name = "maven-repository"
+            url = uri("https://repo.xxjanisxx.dev/releases")
+            credentials {
+                username = project.findProperty("mavenRepositoryUser") as String
+                password = project.findProperty("mavenRepositoryPassword") as String
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            pom {
+                name = "interface-extensions"
+                description = "Some extensions for the Interfaces Kotlin library."
+                url = "https://github.com/xXJanisXx/interface-extensions"
+                scm {
+                    url = "https://github.com/xXJanisXx/interface-extensions"
+                    connection = "scm:git:https://github.com/xXJanisXx/interface-extensions.git"
+                    developerConnection = "scm:git:https://github.com/xXJanisXx/interface-extensions.git"
+                }
+                licenses {
+                    license {
+                        name = "Apache-2.0"
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "xxjanisxx"
+                        name = "Janis K."
+                        email = "xxjanisxx@proton.me"
+                    }
+                }
+            }
+        }
+    }
 }
