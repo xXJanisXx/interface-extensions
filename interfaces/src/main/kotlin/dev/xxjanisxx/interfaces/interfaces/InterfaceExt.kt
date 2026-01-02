654649cf18249b19030900cb72df6d3892af0838
xXJanisXx <Janis.Minecrafto@gmx.de>
2026-01-02 18:27:40 +0100

    feat: added hopper build extension function

--- interfaces/src/main/kotlin/dev/xxjanisxx/interfaces/interfaces/InterfaceExt.kt
+++ interfaces/src/main/kotlin/dev/xxjanisxx/interfaces/interfaces/InterfaceExt.kt
@@ -3,3 +3,7 @@ package dev.xxjanisxx.interfaces.interfaces
 /** Creates a new [DropperInterface] using a [DropperInterfaceBuilder]. */
 inline fun buildDropperInterface(builder: DropperInterfaceBuilder.() -> Unit): DropperInterface =
     DropperInterfaceBuilder().also(builder).build()
+
+/** Creates a new [HopperInterface] using a [HopperInterfaceBuilder]. */
+inline fun buildHopperInterface(builder: HopperInterfaceBuilder.() -> Unit): HopperInterface =
+    HopperInterfaceBuilder().also(builder).build()
