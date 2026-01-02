b82a280c72651439b193dc3428aa1c761db33b0c
xXJanisXx <Janis.Minecrafto@gmx.de>
2026-01-02 17:25:33 +0100

    fix: builder function name

--- interfaces/src/main/kotlin/dev/xxjanisxx/interfaces/interfaces/InterfaceExt.kt
+++ interfaces/src/main/kotlin/dev/xxjanisxx/interfaces/interfaces/InterfaceExt.kt
@@ -1,5 +1,5 @@
 package dev.xxjanisxx.interfaces.interfaces
 
 /** Creates a new [DropperInterface] using a [DropperInterfaceBuilder]. */
-public inline fun buildChestInterface(builder: DropperInterfaceBuilder.() -> Unit): DropperInterface =
+inline fun buildDropperInterface(builder: DropperInterfaceBuilder.() -> Unit): DropperInterface =
     DropperInterfaceBuilder().also(builder).build()
